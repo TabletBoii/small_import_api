13b0fd215cbbccc2fadbe1538729fabd7ed79499
TabletBoii <grand1354ru@gmail.com>
2024-11-05 21:15:23 +0500

    minor changes, added upload_plan_values endpoint

--- pydantic_models/request_models.py
+++ pydantic_models/request_models.py
@@ -7,7 +7,7 @@ class PlanValuesModel(BaseModel):
     partner_town_name: str
     partner_name: str
     partner_category_name: str | None = None
-    partner_add_date: datetime.datetime
+    partner_add_date: str
     partner_internet: bool
     supervisor_name: str | None = None
     partner_parttype_name: str | None = None
