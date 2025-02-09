7084c7c46264594f54f7e50cf9eb278dca62bdb8
TabletBoii <grand1354ru@gmail.com>
2025-02-10 04:57:34 +0500

    cors policy added

--- controllers/claims_inbetween_uploader.py
+++ controllers/claims_inbetween_uploader.py
@@ -76,7 +76,8 @@ class ClaimsInBetweenUploader:
         timezone = pytz.timezone("Asia/Karachi")
 
         formated_till_date = datetime.now(timezone).strftime("%Y-%m-%d %H:%M")
-
+        print(formated_from_date)
+        print(formated_till_date)
         unconfirmed_claims_query = self.get_formated_query(formated_from_date, formated_till_date)
         fetched_data = await self.__execute_query(unconfirmed_claims_query)
         return self.process_result_data(fetched_data)
